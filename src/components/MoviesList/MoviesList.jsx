1f54d1db7b17a707c534ee43e58b4f9ed2a245ec
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-09-15 18:57:10 +0300

    commit

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const MoviesList = ({ movies }) => {
+const MoviesList = ({ movies, state }) => {
   return (
     <ul>
       {movies.map(movie => (
         <li key={movie.id}>
-          <Link to={`${movie.id}`}>{movie.title}</Link>
+          <Link to={`${movie.id}`} state={state}>
+            {movie.title}
+          </Link>
         </li>
       ))}
     </ul>
